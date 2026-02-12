ecc6d826af1183b6b266328b48d12bccb8c88d2c
D1egoSebastian <sebastiandiegorr@gmail.com>
2026-02-12 15:20:41 -0400

    authcontroller and token function

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,9 +1,14 @@
 ﻿using BCrypt.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using TaskManagerAPI.Data;
 using TaskManagerAPI.DTOs;
 using TaskManagerAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace TaskManagerAPI.Controllers
 {
@@ -17,9 +22,12 @@ namespace TaskManagerAPI.Controllers
         */
 
         private readonly AppDbContext _context;
-        public AuthController(AppDbContext context)
+        private readonly IConfiguration _configuration;
+        public AuthController(AppDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
+
         }
 
         [HttpPost("register")]
@@ -69,6 +77,12 @@ namespace TaskManagerAPI.Controllers
         {
             try
             {
+
+                if(dto.Email == null || dto.Password == null)
+                {
+                    return BadRequest();
+                }
+
                 var user = _context.Users.FirstOrDefault( x => x.Email == dto.Email);
                 if (user == null)
                 {
@@ -80,8 +94,45 @@ namespace TaskManagerAPI.Controllers
                     return Unauthorized();
                 }
 
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.Name)
+                };
 
-                //Realizar lo del token
+                //inyectar IConfiguration 
+                var key = _configuration["Jwt:Key"];
+                var issuer = _configuration["Jwt:Issuer"];
+                var audience = _configuration["Jwt:Audience"];
+                var expiresMinutes = int.Parse(_configuration["Jwt:ExpiresMinutes"] ?? "60");
+
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var Token = new JwtSecurityToken(
+                        issuer: issuer,
+                        audience: audience,
+                        claims: claims,
+                        expires: DateTime.UtcNow.AddMinutes(expiresMinutes),
+                        signingCredentials: credentials
+                    );
+
+
+                return Ok(new
+                {
+                    token = Token,
+                    user = new
+                    {
+                        id = user.Id,
+                        name = user.Name,
+                        email = user.Email,
+                    }
+                }
+
+                 );
+
+                
 
             }catch (Exception ex)
             {
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using TaskManagerAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,28 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+        jwtOPtions =>
+        {
+            jwtOPtions.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true,
+
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+
+                IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+
+                ClockSkew = TimeSpan.Zero
+            };
+        }
+    );
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -21,6 +46,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 var summaries = new[]
